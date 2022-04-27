cacdeff801dcd7fec943a3d603efdcbe2207f3ed
yuxiqian <akaza_akari@sjtu.edu.cn>
2022-04-27 21:59:48 +0800

    [add] tweet now!

--- koke-kokko/src/views/HomeView.tsx
+++ koke-kokko/src/views/HomeView.tsx
@@ -1,7 +1,20 @@
+import {Button, FormControlLabel, Stack, Switch, TextField} from "@mui/material";
+
 function HomeView() {
     return (
         <div>
-            HomeView.
+            <Stack spacing={2} sx={{ maxWidth: 400 }}>
+                <TextField
+                    id="outlined-textarea"
+                    label="What's happening?"
+                    placeholder="Input here"
+                    multiline
+                />
+                <Stack direction="row" justifyContent="flex-end">
+                    <FormControlLabel control={<Switch defaultChecked color="primary" />} label="Visible to Everyone" />
+                    <Button variant="outlined" >Kokko!</Button>
+                </Stack>
+            </Stack>
         </div>
     )
 }
