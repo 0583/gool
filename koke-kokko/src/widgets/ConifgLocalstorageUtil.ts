406678d604fdd4b63117318bd9519a4fec88fc0e
jiahao xu <58067154+ian-hui@users.noreply.github.com>
2022-06-06 10:36:41 +0800

    [fix]class to interface

--- koke-kokko/src/widgets/ConifgLocalstorageUtil.ts
+++ koke-kokko/src/widgets/ConifgLocalstorageUtil.ts
@@ -1,6 +1,5 @@
 
 import { Config } from '../services/service';
-import { csdi } from "../services/proto/koke_kokko";
 
 export namespace LocalStoreConfig {
     export function set_config(config: Config) {
@@ -10,13 +9,7 @@ export namespace LocalStoreConfig {
     export function get_config(): Config | null {
         let conifg_str = localStorage.getItem('config');
         if (conifg_str) {
-            let configFromStorage = JSON.parse(conifg_str) as Config;
-            let config = new Config()
-            let user = new csdi.User()
-            Object.assign(user, configFromStorage['user'])
-            Object.assign(config, configFromStorage)
-            config.user = user
-            return config
+            return JSON.parse(conifg_str);
         }
         else {
             return null;
